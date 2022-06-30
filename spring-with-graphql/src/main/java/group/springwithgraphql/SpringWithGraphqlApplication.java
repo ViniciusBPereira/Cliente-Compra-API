d0b6c26649e9ba4af538ae0d99b9867ee66d7aed
viniciusbpereira <viniciusbegosso@gmail.com>
2022-06-30 20:59:02 -0300

    Starter

---
+++ spring-with-graphql/src/main/java/group/springwithgraphql/SpringWithGraphqlApplication.java
@@ -0,0 +1,13 @@
+package group.springwithgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithGraphqlApplication.class, args);
+	}
+
+}
