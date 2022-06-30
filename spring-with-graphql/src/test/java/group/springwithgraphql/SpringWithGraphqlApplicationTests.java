782b526b8a4a4464820d1369dc7fbb43ecd951ef
viniciusbpereira <viniciusbegosso@gmail.com>
2022-06-30 20:58:42 -0300

    Tests

---
+++ spring-with-graphql/src/test/java/group/springwithgraphql/SpringWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package group.springwithgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
